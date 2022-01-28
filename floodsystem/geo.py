b3d527e6b61b1f6b23ec1d66211d3baf55a55926
ParthVader02 <pp537@cam.ac.uk>
2022-01-28 10:28:59 +0000

    added Task 1D.1

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,5 +5,9 @@
 geographical data.
 
 """
-
 from .utils import sorted_by_key  # noqa
+
+#Task D.1
+def rivers_with_station(stations):
+    return {station.river for station in stations if station.name is not None}
+
