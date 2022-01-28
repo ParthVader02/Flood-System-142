b0e4dfa19be87578056e4f8baca64b6a91f41bc8
ParthVader02 <pp537@cam.ac.uk>
2022-01-28 11:08:00 +0000

    Tests for 1D complete

--- test_geo.py
+++ test_geo.py
@@ -1,11 +1,17 @@
-from floodsystem.geo import rivers_with_station
-from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.geo import rivers_with_station 
+from floodsystem.geo import stations_by_river
 from floodsystem.stationdata import build_station_list
 
 
 def test_rivers_with_station():
     stations = build_station_list()
-    river_names = rivers_with_station(stations)
-    print(len(river_names))
-    print(river_names)
-test_rivers_with_station()
+    river_names =list(rivers_with_station(stations))
+    for i in range(len(river_names)):
+        assert river_names[i] != river_names[i-1]
+
+def test_stations_by_river():
+    stations = build_station_list()
+    objects = ((station.station_id, station.measure_id, station.coord, station.typical_range, station.town) for station in stations )
+    rivers = stations_by_river(stations)
+    assert rivers.values() == objects
+
