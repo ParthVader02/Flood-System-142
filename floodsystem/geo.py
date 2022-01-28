c624fce7c0b7256e5400ead956d79ac41fdfb90c
ParthVader02 <pp537@cam.ac.uk>
2022-01-28 11:31:29 +0000

    Completed 1D modules

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,7 +7,9 @@ geographical data.
 """
 from .utils import sorted_by_key  # noqa
 
-#Task D.1
+#Task D
 def rivers_with_station(stations):
     return {station.river for station in stations if station.name is not None}
 
+def stations_by_river(stations):
+    return {station.river: (station.station_id, station.measure_id, station.coord, station.typical_range, station.town) for station in stations}
