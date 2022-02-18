15ad3a2d01fe97ba0f0134980be2d6e943f37f1b
ParthVader02 <pp537@cam.ac.uk>
2022-02-18 20:17:32 +0000

    Task 2F - doesn't show plots for some reason

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,14 +1,16 @@
 from optparse import Values
 import matplotlib.pyplot as plt
 from floodsystem.analysis import polyfit
-
+import matplotlib
+import numpy as np
 
 def plot_water_level_with_fit(station, dates, levels, p):
-    poly, d0 = polyfit(dates, levels, p)
+    x = matplotlib.dates.date2num(dates)
+    poly, d0 = polyfit(x, levels, p)
 
-    plt.plot(dates, levels, '.')
+    plt.plot(x, levels, '.')
 
-    x1 = np.linspace(dates[0], dates[-1], 30)
+    x1 = np.linspace(x[0], x[-1], 30)
 
     plt.plot(x1, poly(x1))
     plt.show()
