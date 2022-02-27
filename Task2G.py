ee6369f3bd6bf2115a69b4ff7ba6235c7873a168
ParthVader02 <pp537@cam.ac.uk>
2022-02-27 08:50:16 +0000

    Completed Task2G

--- Task2G.py
+++ Task2G.py
@@ -1,4 +1,5 @@
 import datetime
+from floodsystem.plot import plot_water_level_with_fit
 import numpy as np
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.flood import stations_highest_rel_level
@@ -14,23 +15,27 @@ def run():
     high = []
     moderate = []
     low = []    
-    
+    #goes through all stations
     for station in stations:
         dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=2))
         if not levels:
-            print("No available data")
+            pass
         else:
+            #finds gradient
             rate = np.gradient(levels)
+            #sum gradient to get what the general increase is 
             grad_sum = sum(rate)
+
             if grad_sum < 0.01:
-                low.append(station.name)
+                low.append((station.town, grad_sum))
             elif grad_sum >=0.01 and grad_sum < 0.1:
-                moderate.append(station.name)
+                moderate.append((station.town, grad_sum))
             elif grad_sum >= 0.1 and grad_sum < 0.7:
-                high.append(station.name)
+                high.append((station.town, grad_sum))
             elif grad_sum >= 0.7:
-                severe.append(station.name)
-    print(severe)
+                severe.append((station.town, grad_sum))    
+    severe.sort()        
+    print(severe[0:4])
 
 if __name__ == "__main__":
     run()
